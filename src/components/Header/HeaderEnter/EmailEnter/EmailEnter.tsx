4e1a6a719258cec3de6304d5fcd5698fe57f4110
nastyanikitinteam <nastya@nikitinteam.com>
2022-06-15 20:38:51 +0200

    edit main section

--- src/components/Header/HeaderEnter/EmailEnter/EmailEnter.tsx
+++ src/components/Header/HeaderEnter/EmailEnter/EmailEnter.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link';
 import styles from './emailEnter.module.scss';
 
 const EmailEnter = () => {
   return (
     <div className={styles.block}>
       <input type="text" className={styles.input} placeholder="Email" />
-      <div className={styles.button}>Log in</div>
+      <div className={styles.button}>
+        <Link href="123">
+          <a className="button button-sm">Log in</a>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -1,25 +1,31 @@
-import styles from './mainSection.module.scss';
-
+import Link from 'next/link';
 import Scheme from './Scheme/Scheme';
+
+import styles from './mainSection.module.scss';
 
 const MainSection = () => {
   return (
     <div className={styles.section}>
-      <div className="wrapper">
+      <div className="wrapper wrapper-big">
         <h1 className={styles.title}>
-          We give you access to a sea of crypto data.
+          We give you access <br /> to a sea of crypto data.
         </h1>
         <h2 className={styles.subtitle}>
           <span className="yellowBlock">Build your own apps</span> using this
-          data for your automation, analytics through standard development tools
-          such as <span className={styles.yellow}>Rest API</span> /
+          data for your automation, <br /> analytics through standard
+          development tools such as{' '}
+          <span className={styles.yellow}>Rest API</span> /
           <span className={styles.red}>gRPC</span> /
           <span className={styles.blue}>JDBC</span>.
         </h2>
         <div className={styles.scheme}>
           <Scheme />
         </div>
-        <div className={styles.button}>Create free project</div>
+        <div className={styles.button}>
+          <Link href="/123">
+            <a className="button">Create free project</a>
+          </Link>
+        </div>
       </div>
     </div>
   );
